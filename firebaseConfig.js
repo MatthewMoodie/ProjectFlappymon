dce150cb0f93e412615bfffcdd320a15af3ff079
Christopher Sooknanan <chrisviv016@gmail.com>
2025-04-19 00:36:36 -0400

    Firebase stuff
    I have uploaded the site to firebase but the login is not working currently

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCb9izNoFt9RlL4C44XZC3cWynM8BXyTkU",
+    authDomain: "info-1601-project-2b4bb.firebaseapp.com",
+    projectId: "info-1601-project-2b4bb",
+    storageBucket: "info-1601-project-2b4bb.firebasestorage.app",
+    messagingSenderId: "989604980668",
+    appId: "1:989604980668:web:9b60bcfa061c83570fadb9"
+  };
+
+
+  export default firebaseConfig;
